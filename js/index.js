857084157202049177f59e026fb74afb9635ad7f
sanuel <sanuel@Nitro5SB>
2020-08-16 22:27:11 +0200

    almost done

--- js/index.js
+++ js/index.js
@@ -1,7 +1,5 @@
 let plx = new Parallax(document.getElementById('root'), {
-    pointerEvents: true,
-    invertX: false,
-    invertY: false
+    pointerEvents: true
 });
 
 function delay(t, v) {
@@ -12,56 +10,124 @@ function delay(t, v) {
 
 
 $(() => {
-    // check for dynamic link
-    if (window.location.hash)
-        return loadPageQuick(window.location.hash.replace('#', ''));
+    let imgsrcs = [
+        "img/thumbnails/Canon_iPF.JPG",
+        "img/thumbnails/estefold2300.JPG",
+        "img/thumbnails/OCEplotwave450.JPG",
+        "img/thumbnails/P1100477.JPG",
+        "img/thumbnails/P1110173a.JPG",
+        "img/thumbnails/P1110174a.JPG",
+        "img/thumbnails/P1110178a.JPG",
+        "img/thumbnails/P1110181a.JPG",
+        "img/thumbnails/P1110182a.JPG",
+        "img/thumbnails/P1110186a.JPG",
+        "img/thumbnails/P1110189a.JPG",
+        "img/thumbnails/SharpMX3060.JPG",
+        "img/thumbnails/sidlo.JPG",
+        "img/thumbnails/Synergix_scanner.JPG",
+        "img/thumbnails/Xerox6204.JPG",
+        "img/thumbnails/Xerox6204printer.JPG"
+    ];
+    for (let imgsrc of imgsrcs) {
+        $('#imagelayer').append(
+            $('<div class="sector sectorimage"></div>').append(
+                $('<img />')
+                .attr('src', imgsrc)
+                .addClass('img-fluid')
+            )
+            .css({
+                left: 'calc((100vw - 250px) * ' + Math.random() + ')',
+                'animation-delay': Math.random() * 100 + 's'
+            })
+        );
+    }
 
-    // replace static linking
-    $('a[data-link]').each((_, el) => {
-        $(el).attr('href', '#' + $(el).attr('data-link'));
+    $('#imagelayer').on('click', '.sectorimage', function(e) {
+        $('#popup-wrapper').toggle();
+        $('#popup-img').attr('src', e.target.src);
     })
 
-    $('body').on('click', 'a', function(e) {
-        loadPage(e.target.href.replace(/.*#/, ''));
+    $('#popup-btn').on('click', function(e) {
+        $('#popup-wrapper').toggle();
+    })
+
+
+    $('#footer-button').on('click', function() {
+        $('.footer')
+            .toggleClass('hidden');
+
+        $('i', $('#footer-button'))
+            .toggleClass('fa-chevron-up')
+            .toggleClass('fa-chevron-down');
+    });
+
+    // check for dynamic link on load
+    if (window.location.hash && !loadPage(window.location.hash.replace('#', '')))
+        return;
+
+    // check for dynamic link on history change
+    window.onpopstate = function(e) {
+        console.log("popstate");
+        loadPage(window.location.hash.replace('#', ''));
+    }
+
+    // replace static links
+    $('a[data-link]').each((_, el) => {
+        console.log("replacing");
+        $(el).attr('href', '#' + $(el).attr('data-link'));
+    });
+
+    $('body').on('click', 'a[data-link]', function(e) {
+        loadPage(e.target.getAttribute("data-link"), { force: true });
     })
 })
 
-function loadPage(pagename) {
-    console.log("Loading page ", pagename);
+let pageCheck = 'none';
+const fadeTime = 500;
+const transitionTime = 1500;
+// options[force] indicates if the change is guaranteed
+function loadPage(pagename, options = {}) {
 
-    if (pagename === 'main') {
+    console.log("Trying to load page ", pagename);
+
+    if (!options["force"] && pageCheck === pagename)
+        return false;
+
+    console.log("Force unset or page differs from actual one");
+
+    pageCheck = pagename;
+
+    if (!pagename || pagename === "home") {
+        $('#subpage-wrapper').hide(fadeTime);
         $('#loadscreen')
             .addClass('reverse')
-            .fadeIn(250);
-        delay(1000).then(() => {
+            .fadeIn(fadeTime);
+        setTimeout(() => {
+            $('#main').remove();
             $('#subpage').remove();
-            $('#loadscreen').fadeOut(250);
-            $('#root').fadeIn(250);
-        });
-
+            $('#loadscreen').fadeOut(fadeTime);
+            $('#root').fadeIn(fadeTime);
+            $('.sector.selected').removeClass('selected');
+        }, transitionTime);
     } else {
-        $('#' + pagename + '-sector').addClass('selected');
-        $('#root').fadeOut(500);
+        $('#' + pagename + '-sector')
+            .addClass('selected');
+
+        $('#root').fadeOut(fadeTime);
         $('#loadscreen')
             .removeClass('reverse')
-            .fadeIn(500);
+            .fadeIn(fadeTime);
 
         setTimeout(() => {
             $('<div/>').load('subpages/' + pagename + '.htm div#main', function() {
                 console.log(this);
-                $('body').append($(this).children());
+                $('#subpage-wrapper')
+                    .append($(this).children())
+                    .show(fadeTime);
             });
-            $('#loadscreen').fadeOut(500);
-        }, 2000);
+            $('#loadscreen').fadeOut(fadeTime);
+        }, transitionTime);
     }
-}
 
-function loadPageQuick(pagename) {
-    console.log("Quickloading page", pagename);
-    $('#loadscreen').show();
-    $('#root').hide();
-    $('<div/>').load('subpages/' + pagename + '.htm div#main', function() {
-        $('#loadscreen').hide();
-        $('body').append($(this).children());
-    });
+    return true;
 }
