53e8d328849377e47a6345085acb6fdab1892aee
sanuel <sanuel@Nitro5SB>
2020-08-24 20:49:54 +0200

    more fixes

--- js/index.js
+++ js/index.js
@@ -79,7 +79,7 @@ function loadPage(pagename, options = {}) {
 
         setTimeout(() => {
             $('<div/>').load('subpages/' + pagename + '.htm div#main', function() {
-                $('a', this).each(function(idx, el) {
+                $('a.img-link', this).each(function(idx, el) {
                     // TODO: Fix this godawful workaround
                     el.href = el.href.replace('/img', '/PlanPrint/img');
                 });
